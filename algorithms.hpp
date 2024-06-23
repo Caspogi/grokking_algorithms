357bd77922ca06bdd8e41a601904bb40fe4a33a2
Caspogi <pnfcbtv@mail.ru>
2024-06-24 01:19:12 +0300

    Реализована функция наименьшего элемента массива.

--- algorithms.hpp
+++ algorithms.hpp
@@ -4,4 +4,4 @@ template<typename T>
 int binarySearch(T&& container, auto item);
 
 template<typename T>
-auto findSmallest(T&& arr);
+T selectionSort(T&& arr);
--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,7 +1,11 @@
 #include "algorithms.hpp"
+#include  <vector>
+#include <iostream>
+
+using std::vector;
 
 template<typename T>
-auto findSmallest(T&& arr)
+int findSmallest(T&& arr)
 {
     auto smallest = arr[0];
     int smallest_index = 0;
@@ -12,7 +16,37 @@ auto findSmallest(T&& arr)
             smallest = arr[i];
             smallest_index = i;
         }
-    return smallest_index;
+
     }
+    return smallest_index;
 }
 
+template<typename T>
+T selectionSort(T&& arr)
+{
+    T newArr(arr);
+    int n = newArr.size();
+    int smallest = -1;
+
+    for (int i = 0; i < n; i++)
+    {
+        smallest = findSmallest(arr);
+        newArr[i] = arr[smallest];
+        arr.erase((arr.begin() + smallest));
+    }
+
+    return newArr;
+}
+
+int main()
+{
+    vector<int> arr{2, 5, 7, 3};
+    vector<int> sortArr(arr);
+
+    sortArr = selectionSort(arr);
+
+    for (int n : sortArr) std::cout << n << '\t';
+    std::cout << std::endl;
+
+    return 0;
+}
